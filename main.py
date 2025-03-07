47b1b1b7ecacbc0ba4bd9c000ef22221cd9d91da
Zsomb <zsombor.galgoczi@gmail.com>
2025-03-07 08:48:42 +0100

    "stats.py"

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    frankenstein = get_book_text("books/frankenstein.txt")
+    num_words = get_num_words(frankenstein)
+    print(f"{num_words} words found in the documents")
+
+main()
