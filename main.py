dd9bf04c24ff5077e0a0f76f85106b5225b8697f
Zsomb <zsombor.galgoczi@gmail.com>
2025-03-08 22:36:41 +0100

    Final program

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_char_counts, get_sorted_char_counts
 
 def get_book_text(path):
@@ -6,16 +7,19 @@ def get_book_text(path):
     return file_contents
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: pyhton3 main.py ‹path_to_book›")
+        sys.exit(1)
+    book_path = sys.argv[1]
     print("============ BOOKBOT ============")
-    book = "books/frankenstein.txt"
-    print(f"Analyzing book found at {book}...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    frankenstein = get_book_text(book)
+    book = get_book_text(book_path)
 
-    num_words = get_num_words(frankenstein)
+    num_words = get_num_words(book)
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    char_counts = get_char_counts(frankenstein)
+    char_counts = get_char_counts(book)
     sorted_char_counts = get_sorted_char_counts(char_counts)
     for line in sorted_char_counts:
         print(f"{line["character"]}: {line["count"]}")
